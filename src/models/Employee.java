107c0ac999f0abce4a3148560e5301d264d328f0
Ko <hattoriface@gmail.com>
2021-01-31 14:50:19 +0900

    Add Employee index

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -12,24 +12,23 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Table(name = "employees")
-@NamedQueries({     //SELECT文を入れる
+@NamedQueries({
     @NamedQuery(
-            name = "getAllEmployees", //全ての従業員情報を取得する
-            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"    //SELECT文
-            ),
+        name = "getAllEmployees",
+        query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+    ),
     @NamedQuery(
-            name = "getAllEmployeeCount",   //従業員情報の全件数を取得
-            query = "SELECT COUNT(e) FROM Employee AS e"    //SELECT文
-            ),
+        name = "getEmployeesCount",
+        query = "SELECT COUNT(e) FROM Employee AS e"
+    ),
     @NamedQuery(
-            name = "checkRegisteredCode",   //指定された社員番号がすでにデータベースに存在しているか調べる
-            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"   //SELECT文
-            ),
+        name = "checkRegisteredCode",
+        query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+    ),
     @NamedQuery(
-            name = "checkLoginCodeAndPassword",     //従業員がログインするときに社員番号とパスワードが正しいかどうかチェックする
-            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass" //SELECT文
-            ),
-
+        name = "checkLoginCodeAndPassword",
+        query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+    )
 })
 @Entity
 public class Employee {
@@ -38,17 +37,17 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "code", nullable = false, unique = true) //このunique = trueはすでに存在している社員番号を登録できない旨をデータベースに伝えるもの（＝一意制約）
+    @Column(name = "code", nullable = false, unique = true)
     private String code;
 
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "password", length = 64, nullable = false)//length=64はSHA256というハッシュ関数は64文字の文字列に変換するからここで指定
+    @Column(name = "password", length = 64, nullable = false)
     private String password;
 
     @Column(name = "admin_flag", nullable = false)
-    private Integer admin_frag;
+    private Integer admin_flag;
 
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
@@ -59,7 +58,6 @@ public class Employee {
     @Column(name = "delete_flag", nullable = false)
     private Integer delete_flag;
 
-    //getter/setter
     public Integer getId() {
         return id;
     }
@@ -92,12 +90,12 @@ public class Employee {
         this.password = password;
     }
 
-    public Integer getAdmin_frag() {
-        return admin_frag;
+    public Integer getAdmin_flag() {
+        return admin_flag;
     }
 
-    public void setAdmin_frag(Integer admin_frag) {
-        this.admin_frag = admin_frag;
+    public void setAdmin_flag(Integer admin_flag) {
+        this.admin_flag = admin_flag;
     }
 
     public Timestamp getCreated_at() {
@@ -123,5 +121,4 @@ public class Employee {
     public void setDelete_flag(Integer delete_flag) {
         this.delete_flag = delete_flag;
     }
-
 }
